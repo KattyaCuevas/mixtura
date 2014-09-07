0ba303ae4c9e913f3c3a82f29bac8af503ed2775
Bryam Rodriguez <brodriguezaybar@gmail.com>
2014-09-07 16:53:07 -0500

    Cambiar estructura de views

--- app/app.js
+++ app/app.js
@@ -1,7 +1,28 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('mixtura', [
+var app = angular.module('mixtura', [
   'ngRoute',
   'mixtura.search'
 ]);
+
+app.controller('searchController', ["$http",function($http) {
+
+   this.term = '';
+
+   this.searchApi = function(){
+    if (!this.term) {
+      return;
+    };
+   var search = this;
+   search.videos = [];
+   var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ this.term +"&key=AIzaSyCGBuFPho5UQuaGVhxyV42_kxsodaIdEEY";
+
+   $http.get(url).success(function(data){
+      search.videos = data.items;
+      this.term = "";
+    });
+
+ };
+
+}]);
